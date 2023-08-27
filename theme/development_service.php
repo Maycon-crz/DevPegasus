76795296df35cacec929a597d326061738d8aada
Maycon Nascimento de Oliveira <63969109+Maycon-crz@users.noreply.github.com>
2023-08-27 14:32:14 -0300

    Vídeo

--- theme/development_service.php
+++ theme/development_service.php
@@ -54,6 +54,14 @@
         <h4 class="mt-5">Site no Ar</h4>
         <p>Finalmente, seu site ganha vida! Nós o lançamos oficialmente e comemoramos seu sucesso online.</p>
         <a href="#contato" class="btn btn-outline-info btn-lg">Peça um Orçamento Agora</a>
+        <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;
+            padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+            border-radius: 8px; will-change: transform;">
+            <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+                src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFsvjqmUmA&#x2F;watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+            </iframe>
+        </div>
+        <a href="#contato" class="btn btn-outline-info btn-lg">Peça um Orçamento Agora</a>
     </div>
 </section>
 <section class="row row-project-templates">
@@ -79,7 +87,6 @@
         </div>
     </div>
 </section>
-
 <section class="parallax_development_service mt-3">
     <div id="row_desenvolvimento_aplicativos_para_android" class="container text-center">
         <h2>Aplicativos para Android</h2>
